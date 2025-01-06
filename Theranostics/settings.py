d52acd40c617e2344a4a080ff1bd9f910242dc88
betelgeuse12 <essancio@up.edu.ph>
2025-01-07 01:07:10 +0800

    Updated ALLOWED_HOSTS for Deployment

--- Theranostics/settings.py
+++ Theranostics/settings.py
@@ -145,8 +145,15 @@ STATICFILES_DIRS = (
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-ALLOWED_HOSTS = ['theranostics-production.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://theranostics-production.up.railway.app']
+#ALLOWED_HOSTS = ['theranostics-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['hi191-grp4-theranostics-production.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'https://theranostics-production.up.railway.app',
+    'https://hi191-grp4-theranostics-production.up.railway.app'
+]
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
