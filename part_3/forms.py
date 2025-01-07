58b1246fa991581987b19faba808c3d93170c653
Clark Rodriguez <acrprodriguez05@gmail.com>
2025-01-07 17:36:39 +0800

    Numerical Handling

--- part_3/forms.py
+++ part_3/forms.py
@@ -26,18 +26,19 @@ class EditPostTherapy(ModelForm):
             'with_spect_ct': 'With SPECT/CT',
             'post_therapy_scan_hours': 'Therapy Scan Duration (in Hours)'
         }
-        def clean_salivary_gland(self):
-            salivary_gland = self.cleaned_data['salivary_gland']
-            if salivary_gland < 0:
-                raise forms.ValidationError("Salivary gland must be a positive value")
-            return salivary_gland
-        def clean_kidney_left(self):
-            kidney_left = self.cleaned_data['kidney_left']
-            if kidney_left < 0:
-                raise forms.ValidationError("Left kidney must be a positive value")
-            return kidney_left
-        def clean_kidney_right(self):
-            kidney_right = self.cleaned_data['kidney_right']
-            if kidney_right < 0:
-                raise forms.ValidationError("Right kidney must be a positive value")
-            return kidney_right
+
+    def clean_salivary_gland(self):
+        salivary_gland = self.cleaned_data['salivary_gland']
+        if salivary_gland < 0:
+            raise forms.ValidationError("Salivary gland must be a positive value")
+        return salivary_gland
+    def clean_kidney_left(self):
+        kidney_left = self.cleaned_data['kidney_left']
+        if kidney_left < 0:
+            raise forms.ValidationError("Left kidney must be a positive value")
+        return kidney_left
+    def clean_kidney_right(self):
+        kidney_right = self.cleaned_data['kidney_right']
+        if kidney_right < 0:
+            raise forms.ValidationError("Right kidney must be a positive value")
+        return kidney_right
